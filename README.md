b601b340a185abfc69ca8efe7b4f3a26abc092b9
nokira <mattiadagosta0@gmail.com>
2026-07-10 15:09:37 +0200

    Project finished, ready for Ship #1
    I managed to implement everything I wanted to and I feel like the MainTab project is ready to be shipped!! :D
    
    It took me quite a while to create a decent layout (it’s a bit awful so it will probably change) and implement all the features. Luckly for most problems a quick online research was enough to fix them, while for others it took me quite a while and that’s why I spent nearly 4 hours working on this since last Devlog.
    
    BUT, I managed to do it, and I am very happy because everything works as intended!!
    
    MainTab features:
    News Feed: Get the Top Stories from “HackerNews” right on your page, for easy access
    Weather Forecast: Get current weather conditions and a 7-day forecast!
    GitHub Profile Stats: A little widget that shows your total contributions on GitHub and your current and longest Streak!
    Search: … Well … Obviously, it lets you search anything on the Web using DuckDuckGo.
    Daily Background: Get a different background every day!
    Bookmarks: Save your most used websites for easy access!
    If you want to know all the APIs used here they are:
    
    Open Meteo: Get weather data using Latitude and Longitude.
    BigDataCloud Reverse Geocoding: Get a precise location using Latitude and Longitude.
    Google Favicon API: This API lets you get the Icon of a website. I couldn’t find any official documentation, so here is the guide I used.
    Hacker News API: To get news from the HackerNews Feed.
    Quoteslate API: Get a random quote.
    Bing Wallpaper API: API that lets you get a daily background from the Bing.com
    GitHub Readme Streak Stats: Generate the profile stats card for GitHub
    Also, data like GitHub username and bookmarks are all saved into localStorage so nothing is lost on refresh!!

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# MainTab
-A "New Page" Tab built for Stardance! Hope you like it!
+# What is MainTab
+
+**MainTab** is my own New Tab page made for Stardance. It's built using HTML, CSS and JavaScript using some APIs listed down this page. 
+I decided to build it like this because it has everything I would want from a New Tab page.
+
+<video src="img/mainTabShowcase.mp4" controls width="100%"></video>
+
+---
+
+## MainTab features:
+- **News Feed**: Get the *Top Stories* from "HackerNews" right on your page, for easy access
+- **Weather Forecast**: Get current weather conditions and a *7-day forecast*!
+- **GitHub Profile Stats**: A little widget that shows your *total contributions* on GitHub and your *current* and *longest* Streak!
+- **Search**: ... Well ... Obviously, it lets you search anything on the Web using DuckDuckGo.
+- **Daily Background**: Get a *different background* every day!
+- **Bookmarks**: Save your *most used websites* for easy access!
+
+---
+
+If you want to know all the APIs used here they are:
+
+- [Open Meteo](https://open-meteo.com): Get weather data using Latitude and Longitude.
+- [BigDataCloud Reverse Geocoding](https://www.bigdatacloud.com/reverse-geocoding): Get a precise location using Latitude and Longitude.
+- [Google Favicon API](https://dev.to/derlin/get-favicons-from-any-website-using-a-hidden-google-api-3p1e): This API lets you get the Icon of a website. I couldn't find any official documentation, so here is the guide I used.
+- [Hacker News API](https://github.com/HackerNews/API): To get news from the HackerNews Feed.
+- [Quoteslate API](https://quoteslate.vercel.app/): Get a random quote.
+- [Bing Wallpaper API](https://github.com/TimothyYe/bing-wallpaper): API that lets you get a daily background from the Bing.com
+- [GitHub Readme Streak Stats](https://github.com/DenverCoder1/github-readme-streak-stats): Generate the profile stats card for GitHub
+
+Also, data like GitHub username and bookmarks are all saved into `localStorage` so nothing is lost on refresh!!
+
+## To Run Locally:
+
+1. Clone the Repo:
+    ```
+        git clone https://github.com/n0kira/MainTab
+    ```
+2. Open the website:
+    From your file manager find the folder named `MainTab`, open it and run `index.html`
--- script.js
+++ script.js
@@ -1,7 +1,86 @@
+// Check User's preferences
+const weatherPref = document.getElementById('showWeather');
+const newsPref = document.getElementById('showNews');
+const streakPref = document.getElementById('showStreak');
+const quotePref = document.getElementById('showQuote');
+const streakImg = document.getElementById('githubProfileImg');
+
+let buttons = [weatherPref, newsPref, streakPref, quotePref];
+let preferences = JSON.parse(localStorage.getItem("preferences")) || ["true","true","true","true"];
+
+function checkPref() {
+    const newsFeed = document.getElementById('newsFeed');
+    const quoteSpace = document.getElementById('quote');
+    const weatherFeed = document.getElementById('weatherFeed');
+    const githubProfile = document.getElementById('githubProfile');
+
+    weatherPref.checked = (preferences[0] == "true");
+    newsPref.checked = (preferences[1] == "true");
+    streakPref.checked = (preferences[2] == "true");
+    quotePref.checked = (preferences[3] == "true");
+
+    if (preferences[0] == "true") {
+        weatherFeed.style.visibility = "visible";
+        if (document.getElementById("currentForecast").children.length == 0) {
+            fetchWeather();
+        }
+    } else {
+        weatherFeed.style.visibility = "hidden";
+    }
+
+    if (preferences[1] == "true") {
+        newsFeed.style.visibility = "visible";
+        if (newsFeed.children.length == 0) {
+            fetchNews();
+        }
+    } else {
+        newsFeed.style.visibility = "hidden";
+    }
+
+    if (preferences[2] == "true") {
+        githubProfile.style.visibility = "visible";
+        if (!streakImg.getAttribute("src")) {
+            githubStreak();
+        }
+    } else {
+        githubProfile.style.visibility = "hidden";
+    }
+
+    if (preferences[3] == "true") {
+        quoteSpace.style.visibility = "visible";
+        if (quoteSpace.children.length == 0) {
+            fetchQuote();
+        }
+    } else {
+        quoteSpace.style.visibility = "hidden";
+    }
+}
+
+function updatePref() {
+    
+    preferences[0] = weatherPref.checked.toString();
+    preferences[1] = newsPref.checked.toString();
+    preferences[2] = streakPref.checked.toString();
+    preferences[3] = quotePref.checked.toString();
+
+    checkPref();
+
+    localStorage.setItem("preferences", JSON.stringify(preferences));
+}
+
+buttons.forEach(button => {
+    button.addEventListener("change", () => {
+        updatePref();
+    });
+});
+
+checkPref();
+
 // HackerNews Top Stories Fetch
 const newsFeed = document.querySelector('#newsFeed');
 
-fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
+function fetchNews() {
+    fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
     .then(response => response.json())
     .then(storyIds => {
         const topIds = storyIds.slice(0, 20);
@@ -31,11 +110,12 @@ fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
             storyUrl.href = story.url;
         });
     });
-
+}
 // Random Quote Fetch
 const quoteSpace = document.querySelector('#quote');
 
-fetch('https://quoteslate.vercel.app/api/quotes/random')
+function fetchQuote() {
+    fetch('https://quoteslate.vercel.app/api/quotes/random')
     .then(response => response.json())
     .then(data => {
         const quote = data.quote;
@@ -50,70 +130,129 @@ fetch('https://quoteslate.vercel.app/api/quotes/random')
         quoteText.innerHTML = quote;
         quoteAuthor.innerHTML = "~ "+author;
     });
+}
 
 // Weather API
 
+const weatherFeed = document.getElementById('weatherFeed');
 const currentForecast = document.querySelector('#currentForecast');
 const weeklyForecast = document.querySelector('#weeklyForecast');
 
-if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-        (position) => {
-            const lat = position.coords.latitude;
-            const lon = position.coords.longitude;
+function fetchWeather() {
+    if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                const lat = position.coords.latitude;
+                const lon = position.coords.longitude;
+                weatherFeed.style.visibility = "hidden";
 
-            fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code`)
-                .then(response => response.json())
-                .then(data => {
+                fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,weather_code`)
+                    .then(response => response.json())
+                    .then(data => {
 
-                    const temp = data.current.temperature_2m;
-                    const code = data.current.weather_code;
-                    const weather = translateCode(code);
-                    
-                    const weatherText = currentForecast.appendChild(document.createElement('p'));
-                    weatherText.innerHTML = `Temp: ${temp} - Weather: ${weather}`;
-                });
-
-            fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=auto`)
-                .then(response => response.json())
-                .then(data => {
-
-                    const dates = data.daily.time;
-                    const maxTemps = data.daily.temperature_2m_max;
-                    const minTemps = data.daily.temperature_2m_min;
-                    const codes = data.daily.weather_code;
-
-                    for (let i = 0; i < 7; i++) {
-                        const date = dates[i];
-                        const maxTemp = maxTemps[i];
-                        const minTemp = minTemps[i];
-                        const code = codes[i];
-
+                        const temp = data.current.temperature_2m;
+                        const code = data.current.weather_code;
                         const weather = translateCode(code);
-                        const weatherText = weeklyForecast.appendChild(document.createElement('p'));
+                        
+                        const weatherText = currentForecast.appendChild(document.createElement('div'));
+                        weatherText.classList.add("currentWeatherDiv");
 
-                        weatherText.innerHTML = `Date: ${date} | High: ${maxTemp}°C | Low: ${minTemp}°C | Condition: ${weather}`;
-                    };
-                });
-        },
-        (error) => {
-            console.log("Cannot access user location");
-        }
-    )
+                        const conditionImg = weatherText.appendChild(document.createElement('img'));
+                        conditionImg.src = `img/icons/${weather}`
+                        conditionImg.style.width = "64px";
+
+                        const cityText = weatherText.appendChild(document.createElement('p'));
+
+                        fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${lat}&longitude=${lon}`)
+                            .then(response => response.json())
+                            .then(data => {
+                                cityText.innerHTML = data.city;
+                            });
+                        weatherFeed.style.visibility = "visible";
+                    });
+
+                fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_max,temperature_2m_min,weather_code&timezone=auto`)
+                    .then(response => response.json())
+                    .then(data => {
+
+                        const dates = data.daily.time;
+                        const maxTemps = data.daily.temperature_2m_max;
+                        const minTemps = data.daily.temperature_2m_min;
+                        const codes = data.daily.weather_code;
+
+                        for (let i = 0; i < 7; i++) {
+                            const date = dates[i];
+                            const maxTemp = maxTemps[i];
+                            const minTemp = minTemps[i];
+                            const code = codes[i];
+
+                            const weather = translateCode(code);
+                            const weatherText = weeklyForecast.appendChild(document.createElement('div'));
+                            weatherText.classList.add("weatherDiv");
+
+                            const dateContainer = weatherText.appendChild(document.createElement('div'));
+
+                            const dateText = dateContainer.appendChild(document.createElement('p'));
+                            dateText.innerHTML = `${date.split("-")[2]}/${date.split("-")[1]}`;
+
+                            const conditionContainer = weatherText.appendChild(document.createElement('div'));
+
+                            const conditionImg = conditionContainer.appendChild(document.createElement('img'));
+                            conditionImg.src = `img/icons/${weather}`
+                            conditionImg.style.width = "32px";
+
+                            const tempContainer = weatherText.appendChild(document.createElement('div'));
+
+                            const maxTempText = tempContainer.appendChild(document.createElement('p'));
+                            maxTempText.innerHTML = `${maxTemp}°C`
+
+                            const minTempText = tempContainer.appendChild(document.createElement('p'));
+                            minTempText.innerHTML = `${minTemp}°C`
+
+                            weatherFeed.style.visibility = "visible";
+                        };
+                    });
+            },
+            (error) => {
+                console.log("Cannot access user location");
+            }
+        )
+    }
 }
 
 function translateCode(code) {
-    let weather = "Unknown";
 
-    if (code == 0) weather="Sunny";
-    if (code >= 1 && code <= 3) weather="Partly Cloudy";
-    if (code >= 45 && code <= 48) weather="Foggy";
-    if (code >= 51 && code <= 67) weather="Rainy";
-    if (code >= 71 && code <= 77) weather="Snowy";
-    if (code >= 80 && code <= 82) weather="Rain Showers";
-    if (code >= 95 && code <= 99) weather="Thunderstorm";
+    let iconName = "Icon=Sunny.png";
 
-    return weather;
+    if (code === 0) {
+        iconName = "Icon=Sunny.png";
+    } else if (code >= 1 && code <= 3) {
+        iconName = "Icon=Partly Cloudy.png";
+    } else if (code >= 45 && code <= 48) {
+        iconName = "Icon=Cloudy.png";
+    } else if (code >= 51 && code <= 55) {
+        iconName = "Icon=Light Drizzle.png";
+    } else if (code >= 56 && code <= 57) {
+        iconName = "Icon=Sleet.png";
+    } else if (code >= 61 && code <= 65) {
+        iconName = "Icon=Rainy.png";
+    } else if (code >= 66 && code <= 67) {
+        iconName = "Icon=Sleet.png";
+    } else if (code >= 71 && code <= 75) {
+        iconName = "Icon=Snow.png";
+    } else if (code >= 77 && code <= 79) {
+        iconName = "Icon=Snowfall.png";
+    } else if (code >= 80 && code <= 82) {
+        iconName = "Icon=Rainy with Sun.png";
+    } else if (code >= 85 && code <= 86) {
+        iconName = "Icon=Snowfall.png";
+    } else if (code === 95) {
+        iconName = "Icon=Lightning.png";
+    } else if (code >= 96 && code <= 99) {
+        iconName = "Icon=Thunderstorm.png";
+    }
+
+    return iconName;
 }
 
 // Bookmarks
@@ -130,17 +269,17 @@ addBookmark.addEventListener("click", () => {
     const bookmarksCount = document.querySelectorAll(`.bookmark`);
 
     if (bookmarksCount.length <= 4) {
-        const link = "https://" + prompt("Insert bookmark link (without https://): ");
+        let link = prompt("Insert bookmark link (without https://): ");
 
         if (link) {
 
             createBookmark(link);
-
+            
             bookmarkList.push(link)
 
             localStorage.setItem(`bookmark`, JSON.stringify(bookmarkList));
         } else {
-            alert("Bookmark creation failed. Insert a link and a name");
+            alert("Bookmark creation failed. You must insert a link.");
         }
     }
 });
@@ -150,16 +289,17 @@ function createBookmark (link) {
     bookmark.classList.add("bookmark");
 
     const bookmarkText = bookmark.appendChild(document.createElement('a'));
-    bookmarkText.href = link;
+    bookmarkText.href = "https://" + link;
 
     const bookmarkImg = bookmarkText.appendChild(document.createElement('img'));
-    bookmarkImg.src = `https://www.google.com/s2/favicons?sz=64&domain=${link.split("//")[1]}`;
+    bookmarkImg.src = `https://www.google.com/s2/favicons?sz=64&domain=${link}`;
     bookmarkImg.style.width = "64px";
     bookmarkImg.style.height = "64px";
 
-    const delButton = bookmark.appendChild(document.createElement('button'));
-    delButton.innerText = "Del";
+    const delButton = bookmark.appendChild(document.createElement('img'));
+    delButton.altText = "Del";
     delButton.classList.add("delBookmark");
+    delButton.src = "img/delete.png";
     delButton.addEventListener("click", () => {
         bookmark.remove();
         bookmarkList.splice(bookmarkList.indexOf(link), 1);
@@ -168,3 +308,61 @@ function createBookmark (link) {
     });
 
 }
+
+// Fetch daily wallpaper:
+
+fetch("https://bing.biturl.top/")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("body").style.backgroundImage = `url(${data.url})`;
+    });
+    
+    
+// Custom Github Streak
+function githubStreak() {
+    let username = localStorage.getItem("username") || undefined;
+    if (username) {
+        streakImg.src = `https://streak-stats.demolab.com?user=${username}&theme=dark&hide_border=true`
+    }
+}
+
+streakImg.addEventListener("click", () => {
+
+    username = undefined;
+
+    if (!username) {
+        username = prompt("Insert your GitHub username: ");
+    }
+    
+    if (username.trim().length == 0) {
+        alert("Username must be at least 1 character long!");
+    } else {
+        streakImg.src = `https://streak-stats.demolab.com?user=${username}&theme=dark&hide_border=true`
+        localStorage.setItem("username", username);
+    }
+});
+
+// Search... Duh
+
+const input = document.getElementById('searchInput');
+
+input.addEventListener("keypress", (event) => {
+    const search = input.value;
+    window.location.href = `https://duckduckgo.com/${search}`;
+});
+
+
+// Edit page preferences
+
+const editButton = document.getElementById('editButton');
+const prefWindow = document.getElementById('prefWindow');
+
+prefWindow.style.display = "none";
+
+editButton.addEventListener("click", () => {
+    if (prefWindow.style.display == "none") {
+        prefWindow.style.display = "block";
+    } else {
+        prefWindow.style.display = "none";
+    }
+});
